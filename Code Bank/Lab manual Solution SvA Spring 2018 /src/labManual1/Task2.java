321876568c19c791cb54831c7ed647b503ffb048
Saif Mohammed <saifmohammednipun@gmail.com>
2023-01-11 02:05:04 +0600

    Add files via upload

---
+++ Code Bank/Lab manual Solution SvA Spring 2018 /src/labManual1/Task2.java
@@ -0,0 +1,18 @@
+/*
+ * Task - 2
+(Display five messages) Write a program that displays Welcome to Java five times.
+ */
+package labManual1;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=4; i++) 
+		{
+			System.out.println("Welcom to Java five times");
+		}
+
+	}
+
+}
