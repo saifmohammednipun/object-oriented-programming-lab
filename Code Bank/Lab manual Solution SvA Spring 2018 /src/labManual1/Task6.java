756479aacb5074fd22bec803a3bb6ec3495542bc
Saif Mohammed <saifmohammednipun@gmail.com>
2023-01-16 03:02:08 +0600

    Update Task6.java

--- Code Bank/Lab manual Solution SvA Spring 2018 /src/labManual1/Task6.java
+++ Code Bank/Lab manual Solution SvA Spring 2018 /src/labManual1/Task6.java
@@ -1,15 +1,16 @@
 /*
  * 
 Task – 6
-(Financial application: calculate tips) Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total. For example, if the user enters 10 for subtotal and 15% for gratuity rate, the program displays $1.5 as gratuity and $11.5 as total. Here is a sample run:
+(Financial application: calculate tips) Write a program that reads the subtotal and the gratuity rate, 
+then computes the gratuity and total. For example, 
+if the user enters 10 for subtotal and 15% for gratuity rate, 
+the program displays $1.5 as gratuity and $11.5 as total. 
 
+Here is a sample run:
 Enter the subtotal and a gratuity rate: 10 15
 The gratuity is $1.5 and total is $11.5
 
  */
-
-
-
 
 package labManual1;
 import java.util.*;
