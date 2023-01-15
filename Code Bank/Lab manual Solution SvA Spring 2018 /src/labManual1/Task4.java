97e179d73d7c47ae741031034e25f5ca6822bc15
Saif Mohammed <saifmohammednipun@gmail.com>
2023-01-16 03:00:30 +0600

    Update Task4.java

--- Code Bank/Lab manual Solution SvA Spring 2018 /src/labManual1/Task4.java
+++ Code Bank/Lab manual Solution SvA Spring 2018 /src/labManual1/Task4.java
@@ -1,5 +1,6 @@
 /*Task – 4
-(Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30 seconds. Write a program that displays the average speed in miles per hour. (Note that 1 mile is 1.6 kilometers.)
+(Average speed in miles) Assume a runner runs 14 kilometers in 45 minutes and 30 seconds. 
+Write a program that displays the average speed in miles per hour. (Note that 1 mile is 1.6 kilometers.)
 */
 
 package labManual1;
